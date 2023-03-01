b62192f18a8dfa812814ed470326d3c3305d67d4
Wing Sun Cheung <brendon.cheung@outlook.com>
2023-03-01 10:08:17 -0500

    changed backend port to 300

--- src/app/purchasing/services/purchase-request-repository.ts
+++ src/app/purchasing/services/purchase-request-repository.ts
@@ -14,9 +14,12 @@ export class PurchaseRequestRepository {
 
     constructor(private http: HttpClient) {
         console.log("isDevMode: ", isDevMode())
-        this.host = isDevMode() ? "localhost" : ''
-        this.port = isDevMode() ? '3000' : ''
+        this.host = isDevMode() ? "localhost" : '192.168.0.127'
+        this.port = isDevMode() ? '3000' : '3000' // port to your backend
         this.baseUrl = `http://${this.host}:${this.port}/api/purchase/`
+        console.log("Host: ", this.host)
+        console.log("Port: ", this.port)
+        console.log("URL: ", this.baseUrl)
     }
 
     public getRequests(): Observable<[PurchaseRequest]> {
